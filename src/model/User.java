7a9b1ccbd08e68971d67779f0b35fc5a86ac775d
Tâmara Thais <t.thais@escolar.ifrn.edu.br>
2025-02-23 20:00:41 -0300

    Create User.java

---
+++ src/model/User.java
@@ -0,0 +1,25 @@
+package model;
+
+public abstract class User {
+    private String id;
+    private String name;
+    private String email;
+    private String cpf;
+
+    public User(String name, String email, String cpf) {
+        this.name = name;
+        this.email = email;
+        this.cpf = cpf;
+        this.id = java.util.UUID.randomUUID().toString();
+    }
+
+    
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+}
+
