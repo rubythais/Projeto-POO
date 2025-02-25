555f76d8e0e26ce8aea765bea74373bb1bfe1af5
Tâmara Thais <t.thais@escolar.ifrn.edu.br>
2025-02-25 00:44:59 -0300

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,6 +1,8 @@
 package model;
 
-public abstract class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String id;
     private String name;
     private String email;
